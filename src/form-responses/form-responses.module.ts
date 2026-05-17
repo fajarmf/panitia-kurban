df6631da2eb5fe9c6c559ad9679188c3cf51cf05
Fajar <fajarmf@gmail.com>
2026-05-18 06:11:44 +0700

    feat(form-responses): add FormResponse entity (JSONB data, unique per pengkurban+form_key)

--- src/form-responses/form-responses.module.ts
+++ src/form-responses/form-responses.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FormResponse } from './form-response.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([FormResponse])],
+})
 export class FormResponsesModule {}
